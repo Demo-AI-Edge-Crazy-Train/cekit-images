651a59fd3907af8a858cdc25b92ccb9de876e3a3
Nicolas Massé <nicolas.masse@itix.fr>
2024-07-02 11:07:07 +0200

    WiP

--- modules/opencv-prereq/configure.sh
+++ modules/opencv-prereq/configure.sh
@@ -12,3 +12,9 @@ cp -pr * /
 popd
 
 createrepo /usr/local/src/opencv/$(uname -m)/
+
+dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf -y install 'dnf-command(config-manager)'
+dnf -y config-manager --enable fedora-cisco-openh264
+dnf -y install ffmpeg
+dnf -y install --disablerepo='*' --enablerepo="rpmfusion-free" --enablerepo="rpmfusion-free-updates" libavcodec-freeworld
